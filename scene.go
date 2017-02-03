ebb47d5f83b945499c4b2d37e6f88a366c33e240
Max Gonzih <gonzih@gmail.com>
2017-02-03 16:14:13 +0100

    Render pipes

--- scene.go
+++ scene.go
@@ -2,17 +2,18 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
 
 	"github.com/veandco/go-sdl2/sdl"
 	img "github.com/veandco/go-sdl2/sdl_image"
 )
 
-// scene stores current game state
 type scene struct {
 	renderer *sdl.Renderer
 	bg       *sdl.Texture
 	bird     bird
+	pipes    pipes
 }
 
 // NewScene creates new scene
@@ -26,7 +27,7 @@ func NewScene(r *sdl.Renderer) (*scene, error) {
 		return s, fmt.Errorf("Error while loading bg: %v", err)
 	}
 
-	s.bird = bird{x: 20, y: windowHeight / 2, w: 50, h: 50, gravity: 1}
+	s.bird = bird{x: 50, y: windowHeight / 2, w: 50, h: 50, gravity: 1}
 
 	for i := 1; i <= 4; i++ {
 		t, err := img.LoadTexture(s.renderer, fmt.Sprintf("resources/bird/frame-%d.png", i))
@@ -36,6 +37,23 @@ func NewScene(r *sdl.Renderer) (*scene, error) {
 		}
 
 		s.bird.frames = append(s.bird.frames, t)
+	}
+
+	s.pipes = pipes{}
+
+	s.pipes.tex, err = img.LoadTexture(s.renderer, "resources/pipe.png")
+
+	if err != nil {
+		return s, fmt.Errorf("Error while loading pipe texture %v", err)
+	}
+
+	for i := 0; i < 10; i++ {
+		s.pipes.pipes = append(s.pipes.pipes, &pipe{
+			pos: windowWidth/2 + int32(rand.Intn(2*windowWidth)),
+			w:   52,
+			h:   int32(rand.Intn(windowHeight / 2)),
+			up:  rand.Intn(10) > 4,
+		})
 	}
 
 	return s, nil
@@ -57,6 +75,11 @@ func (s *scene) run(fps uint32) {
 
 func (s *scene) update() {
 	s.bird.update()
+	s.pipes.update()
+
+	if s.pipes.hits(&s.bird) {
+		s.bird.dead = true
+	}
 
 	if s.bird.dead {
 		s.restart()
@@ -68,11 +91,11 @@ func (s *scene) draw() {
 	s.renderer.Copy(s.bg, &sdl.Rect{X: 0, Y: 0, W: windowWidth, H: windowHeight}, nil)
 
 	s.bird.draw(s.renderer)
+	s.pipes.draw(s.renderer)
 
 	s.renderer.Present()
 }
 
-// bird represents state of birdy
 type bird struct {
 	x, y, w, h     int32
 	speed, gravity int32
@@ -90,8 +113,6 @@ func (b *bird) update() {
 	} else {
 		b.frame = f
 	}
-
-	b.x++
 
 	b.mu.Lock()
 	defer b.mu.Unlock()
@@ -111,4 +132,66 @@ func (b *bird) jump() {
 	b.mu.Lock()
 	defer b.mu.Unlock()
 	b.speed = -10
+}
+
+type pipes struct {
+	pipes []*pipe
+	tex   *sdl.Texture
+}
+
+func (pp *pipes) update() {
+	for _, p := range pp.pipes {
+		p.update()
+	}
+}
+
+func (pp *pipes) draw(r *sdl.Renderer) {
+	for _, p := range pp.pipes {
+		p.draw(r, pp.tex)
+	}
+}
+
+func (pp *pipes) hits(b *bird) bool {
+	for _, p := range pp.pipes {
+		hits := p.hits(b)
+		if hits {
+			return true
+		}
+	}
+
+	return false
+}
+
+type pipe struct {
+	w, h, pos int32
+	up        bool
+}
+
+func (p *pipe) update() {
+	p.pos--
+}
+
+func (p *pipe) draw(r *sdl.Renderer, tex *sdl.Texture) {
+	rect := &sdl.Rect{X: p.pos, Y: windowHeight - p.h, W: p.w, H: p.h}
+
+	flip := sdl.FLIP_NONE
+	if !p.up {
+		rect.H = p.h
+		rect.Y = 0
+		flip = sdl.FLIP_VERTICAL
+	}
+
+	r.CopyEx(tex, nil, rect, 0, nil, flip)
+}
+
+func (p *pipe) hits(b *bird) bool {
+	if b.x+b.w <= p.pos || b.x >= p.pos+p.w {
+		return false
+	}
+
+	if !p.up {
+		return b.y <= p.h
+	}
+
+	return b.y+b.h >= windowHeight-p.h
 }
